a9c160b5a4eafecd95da714548840a9d12dd2912
Rafael Ribeiro <ra20fael04@gmail.com>
2024-10-12 18:16:11 -0300

    refactor: update tailwind's theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,23 @@ export default {
   content: ["./index.html", "./src/**/*.jsx"],
   theme: {
     extend: {
+      screens: {
+        "max-1280": {"max": "1280px"}
+      },
       spacing: {
         "1.5": "0.375rem",
         "2.5": "0.675rem",
         "5.5": "1.375rem"
+      },
+      fontFamily: {
+        rubik: '"Rubik", serif',
+        monospace: "Monospace"
+      },
+      boxShadow: {
+        normal: "rgba(50, 50, 93, 0.25) 0px 25px 50px -20px, rgba(0, 0, 0, 0.3) 0px 15px 30px -30px;"
+      },
+      backgroundImage: {
+        "peaks": "url(/src/assets/layered-peaks.svg) cover"
       },
     },
   },
