a3ee6d309fc4d74963dfdcd2b17fbf53ca354ab8
Rafael Ribeiro <ra20fael04@gmail.com>
2024-10-13 00:53:25 -0300

    incomplete: add new todo field (date).

--- src/todo-app/TodoForm.jsx
+++ src/todo-app/TodoForm.jsx
@@ -9,10 +9,10 @@ let todoId = 0;
 
 
 export function TodoForm({ addTodoDispatcher }) {
-  const [descriptionIsVisible, setDescriptionIsVisible] = useState(false)
+  const [moreDetailtsAreVisible, setMoreDetailsAreVisible] = useState(false)
 
   function handleToggleDescription() {
-    setDescriptionIsVisible(state => !state)
+    setMoreDetailsAreVisible(state => !state)
   }
 
   function handleAddTodo(event) {
@@ -33,13 +33,20 @@ export function TodoForm({ addTodoDispatcher }) {
         <div className="flex items-center gap-3 mb-5">
           <Input type="text" placeholder="título da tarefa" name="todo-name" className="grow" />
           
-          <IconButton type="button" onClick={handleToggleDescription} aria-label="exibir campo de descrição" title="exibir campo de descrição" className="rounded-full"><ChevronDown size={20} className={descriptionIsVisible ? "rotate-180" : ""} /></IconButton>
+          <IconButton type="button" onClick={handleToggleDescription} aria-label="exibir campo de descrição" title="exibir campo de descrição" className="rounded-full"><ChevronDown size={20} className={moreDetailtsAreVisible ? "rotate-180" : ""} /></IconButton>
 
           <IconButton type="submit" aria-label="criar tarefa" title="criar tarefa" className="rounded max-430:hidden"><CornerDownLeft size={28}  /></IconButton>
           <IconButton type="submit" aria-label="criar tarefa" title="criar tarefa" className="rounded hidden max-430:block"><CornerDownLeft size={20}  /></IconButton>
         </div>
 
-        { descriptionIsVisible && <Input textarea placeholder="descrição da tarefa" name="todo-description" className="w-full" />}
+        { 
+          moreDetailtsAreVisible && (
+            <>
+              <Input textarea placeholder="descrição da tarefa" name="todo-description" className="w-full mb-5" />
+              <label>Para quando é? <input type="date" className="bg-transparent border-2 border-zinc-500 p-1 ml-2 rounded-md outline-none focus:border-zinc-300" /></label>
+            </>
+          )
+        }
       </form>
     </>
   )
