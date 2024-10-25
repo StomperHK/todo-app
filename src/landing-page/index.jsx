aab1ad07a966f407207bf06501713705c95516a5
Rafael Ribeiro <ra20fael04@gmail.com>
2024-10-24 21:29:06 -0300

    feat: create landing page.

--- src/landing-page/index.jsx
+++ src/landing-page/index.jsx
@@ -1,27 +1,64 @@
+import { useRef, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 import { Header } from "../components/Header"
 import { Button } from "../components/Button"
+import { UserX, ThumbsUp, WifiOff, ArrowRight} from "react-feather"
 
-import { ArrowRight } from "react-feather"
+import progressiveSVG from "../assets/progressive.svg"
+import styles from "./css/index.module.css"
 
 export function LandingPage() {
+  const {wavesbackground, featuresList} = styles
+
+  const windowEL = document.querySelector("body")
+
+  useEffect(showLandingPage)
+
+  function showLandingPage() {
+
+
+    let transitionClasses = ".opacity-5, .-translate-x-20 .translate-x-20, .-translate-y-20"
+    const hiddenSections = Array.from(document.querySelectorAll(transitionClasses))
+
+    transitionClasses = transitionClasses.replace(/[,.]/g, "").split(" ")
+    hiddenSections.forEach((section) => transitionClasses.forEach(className => section.classList.remove(className)))
+    
+  }
+
   return (
     <>
       <Header />
-      <main>
-        <div className="max-w-6xl m-auto">
-          <figure className="flex gap-10 items-center min-h-[80vh] p-6">
-            <figcaption className="w-[45%]">
-              <h1 className="mb-4">Gerencie suas Tarefas de Forma Simples</h1>
-              <p className="mb-4 text-lg">
+      <main className={`${wavesbackground} full-height-minus-header bg-waves overflow-x-hidden`}>
+        <div className="max-w-7xl m-auto">
+          <figure className="flex justify-evenly gap-10 items-center min-h-[80vh] px-10 pt-16 pb-10 max-1000:flex-col max-1000:min-h-0">
+            <figcaption className="w-[40%] opacity-5 -translate-x-20 transition-transform-opacity delay-300 duration-500 max-1000:w-[100%] max-1000:max-w-[600px]">
+              <h1 className="mb-4 max-520:text-4xl">Gerencie suas Tarefas de Forma Simples</h1>
+              <p className="mb-6 text-lg">
                 Sem login, sem recursos complexos, apenas o essencial para você ser mais produtivo. É tudo tão simples quanto essa página.
               </p>
-              <Link to="/todo-app"><Button className="text-white">começar a usar <ArrowRight size={20} /></Button></Link>
+              <div className="max-700:flex max-700:justify-center">
+                <Link to="/todo-app"><Button className="text-white border-zinc-300">começar a usar <ArrowRight size={20} /></Button></Link>
+              </div>
             </figcaption>
 
-              <img src="../assets/floating.svg" alt="asdw" />
+            <img src={progressiveSVG} className="remove-invert-filter w-[45%] opacity-5 translate-x-20 transition-transform-opacity delay-300 duration-500 max-1000:w-[100%] max-1000:max-w-[600px]" />
           </figure>
+
+          <ul className={`${featuresList} flex justify-center gap-6 items-center px-6 opacity-5 -translate-y-20 transition-transform-opacity delay-500 duration-500 max-700:flex-col max-700:pb-12`}>
+            <li>
+              <UserX size={20} />
+              <h2 className="text-lg">Sem logins chatos</h2>
+            </li>
+            <li>
+              <ThumbsUp size={20} />
+              <h2 className="text-xl">Fácil de usar</h2>
+            </li>
+            <li>
+              <WifiOff size={20} />
+              <h2 className="text-xl">Funciona off-line</h2>
+            </li>
+          </ul>
         </div>
       </main>
     </>
