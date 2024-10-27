e193bcf707b5c1188b9262d1984fbb8c522e5443
Rafael Ribeiro <ra20fael04@gmail.com>
2024-10-27 15:12:33 -0300

    style: minor style changes.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 export function Button({ children, className="", ...props }) {
-  className = "flex items-center gap-2 px-3 py-1.5 border-2 border-zinc-500 bg-zinc-700 rounded transition-colors duration-150 hover:bg-opacity-60" + " " + className
+  className = "flex items-center gap-2 px-3 py-1.5 border-2 border-zinc-500 bg-zinc-700 rounded transition-colors duration-150 hover:bg-opacity-60 focus-visible:bg-opacity-60 focus-visible:border-zinc-200 focus-visible:outline-none" + " " + className
 
   return (
     <button {...props}
