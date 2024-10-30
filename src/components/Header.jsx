48e9f3854264bc4ee8c52592d2a62f0bcad8f4ba
Rafael Ribeiro <ra20fael04@gmail.com>
2024-10-30 20:52:41 -0300

    feat: create PWA install button.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef, forwardRef } from "react"
 import { Link } from "react-router-dom"
 
 import { IconButton } from "./IconButton"
 import { SocialLinks } from "./SocialLinks"
-import { Sun, Moon } from "react-feather"
+import { Download, Sun, Moon } from "react-feather"
 
 import { testLocalStorageAvaliability } from "../lib/testLocalStorageAvaliability"
 
@@ -61,16 +61,63 @@ function ColorThemeSwitch() {
   )
 }
 
+function InstallPWAButton() {
+  const installButtonRef = useRef(null)
+
+  let installPrompt = null
+
+
+  useEffect(storePWAInstallPrompt, [])
+  useEffect(removePWAButtonWhenInstalled, [])
+
+
+  function storePWAInstallPrompt() {    
+    window.addEventListener("beforeinstallprompt", async (e) => {
+      e.preventDefault()
+      installPrompt = e
+
+      installButtonRef.current.removeAttribute("hidden")
+    })
+  }
+
+
+  function removePWAButtonWhenInstalled() {
+    window.addEventListener('appinstalled', () => {
+      installButtonRef.current.setAttribute("hidden")
+      installPrompt = null
+      
+    })
+  }
+
+
+  async function executePWAInstallationPrompt() {
+    if (!installPrompt) return
+
+    const result = await installPrompt.prompt()
+
+    console.log(result.outcume);
+    
+  }
+
+  return (
+    <IconButton ref={installButtonRef} title="instalar aplicativo" className="rounded-full active:bg-zinc-500" hidden onClick={executePWAInstallationPrompt}>
+      <Download  size={20}/>
+    </IconButton>
+  )
+}
+
 export function Header() {
   return (
     <header className="px-5 py-4">
       <div className="flex justify-between items-center max-w-5xl m-auto">  {/* flex-wrapper */}
         <Link to="/" className="flex items-center gap-2 text-white hover:text-white"> TODO APP</Link>
-        <nav className="flex gap-4">
+        <div className="flex gap-4">
           <SocialLinks />
 
           <ColorThemeSwitch />
-        </nav>
+
+          <InstallPWAButton />
+        </div>
       </div>
     </header>
   )
--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,9 +1,12 @@
-export function IconButton({ children, className="",  sm=false, ...props }) {
+import { forwardRef } from "react"
+
+
+export const IconButton = forwardRef(function IconButton({ children, className="",  sm=false, ...props }, ref) {
   className = `${sm ? "p-1" : "p-2"} border-2 rounded border-zinc-500 bg-zinc-800 transition-colors duration-150  active:bg-opacity-40 focus-visible:bg-opacity-60 focus-visible:outline-none focus-visible:border-zinc-200` + " " + className
 
   return (
-    <button className={className} {...props}>
+    <button ref={ref} className={className} {...props}>
       {children}
     </button>
   )
-}
+})
