b081ccee0117a6d2a75075ae7e69b036ed3b1cd6
Rafael Ribeiro <ra20fael04@gmail.com>
2024-10-30 22:36:53 -0300

    attempt: cache scg image.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gka52q266"
+    "revision": "0.bq0kefgmu7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
